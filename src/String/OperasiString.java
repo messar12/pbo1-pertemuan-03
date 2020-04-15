ce58c67763a94f6cf76aa6d6bd22ed7810883c25
messar12 <62458626+messar12@users.noreply.github.com>
2020-04-15 19:10:08 +0800

    TopUppercase untuk membuat huruf menjadi huruf besar tolowercase untuk membuat huruf menjadi huruf kecil substring(5) untuk mengambil nilai string sebnyak 5 huruf dari huruf yg paling akhir substring (0,4) untuk mengambil nilai string sebnyak 4 huruf dari huruf yg pertama dari sebelah kiri

--- src/String/OperasiString.java
+++ src/String/OperasiString.java
@@ -4,5 +4,14 @@ public class OperasiString {
     public static void main(String[] args) {
         String Kota = "Yogyakarta";
         System.out.println(Kota);
+
+        char[] UniskaChar = {'U','N','I','S','K','A'};
+        String UniskaString = new String(UniskaChar);
+        System.out.println(UniskaString);
+
+        System.out.println(Kota.toUpperCase());
+        System.out.println(Kota.toLowerCase());
+        System.out.println(Kota.substring(5));
+        System.out.println(Kota.substring(0,4));
     }
 }
